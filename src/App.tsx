75f0eb0c769a278b830ac07c82916d75611b1c62
YouYou94 <ybh4115@gmail.com>
2022-12-16 17:54:18 +0900

    :memo: App.tsx에서 AppRouter Export

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
 import './App.css';
+import AppRouter from './router/AppRouter';
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <AppRouter />
+    </div>
+  );
 }
 
 export default App;
