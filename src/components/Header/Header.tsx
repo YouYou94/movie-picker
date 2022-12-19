d2cf21c4392e27f2a0bff0cbee3993018bfd1600
YouYou94 <ybh4115@gmail.com>
2022-12-19 16:12:25 +0900

    :memo: Header Styled 코드 추가 및 레이아웃 구현

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,5 +1,15 @@
 import * as Styled from './Styled';
 
 export default function Header() {
-  return <Styled.HeaderLayout>헤더</Styled.HeaderLayout>;
+  return (
+    <Styled.HeaderLayout>
+      <Styled.HeaderBox>
+        <Styled.HeaderTitleBox>
+          <Styled.HeaderTitle>나홀로티비쇼</Styled.HeaderTitle>
+        </Styled.HeaderTitleBox>
+        <Styled.HeaderNavBox>헤더 네비</Styled.HeaderNavBox>
+        <Styled.HeaderSearchBox>헤더 서치</Styled.HeaderSearchBox>
+      </Styled.HeaderBox>
+    </Styled.HeaderLayout>
+  );
 }
