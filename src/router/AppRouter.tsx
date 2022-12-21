4dbb6261ce09695b5770daa8bbd2f56b184e4914
YouYou94 <ybh4115@gmail.com>
2022-12-21 17:03:00 +0900

    :memo: 라우터 코드 삭제

--- src/router/AppRouter.tsx
+++ src/router/AppRouter.tsx
@@ -1,16 +1,9 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Header from '../components/Header/Header';
-import TopBar from '../components/TopBar/TopBar';
-import Home from '../pages/Home/Home';
 
 export default function AppRouter() {
   return (
     <BrowserRouter>
-      <TopBar />
-      <Header />
-      <Routes>
-        <Route path="/tvshow-alone" element={<Home />} />
-      </Routes>
+      <Routes>a</Routes>
     </BrowserRouter>
   );
 }
