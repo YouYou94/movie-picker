269a631d722996d26f89f0b53755e9a5bbfd993b
YouYou94 <ybh4115@gmail.com>
2023-01-20 19:38:29 +0900

    Feat: 영화 데이터 axios로 받아오기 구현

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,20 @@
-import './App.css';
+import { useState, useEffect } from 'react';
 import AppRouter from './router/AppRouter';
+import './App.css';
+import axios from 'axios';
 
 function App() {
+  const [movies, setMovies] = useState();
+
+  useEffect(() => {
+    axios
+      .get(
+        'https://api.themoviedb.org/3/movie/550?api_key=69c4823b56b37a91a1ea5845cce9ce43',
+      )
+      .then((res) => console.log(res.data))
+      .catch((error) => console.log(error));
+  }, []);
+
   return (
     <div className="App">
       <AppRouter />
