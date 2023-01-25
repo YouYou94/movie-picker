98bab9b7cd2747ee4cd723ab837193467f3bf9c0
YouYou94 <ybh4115@gmail.com>
2023-01-25 17:37:23 +0900

    Feat: 커스텀 훅 useAxios 구현

---
+++ src/hooks/useAxios.tsx
@@ -0,0 +1,39 @@
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+
+axios.defaults.baseURL = 'https://api.themoviedb.org';
+
+const TMDB_KEY = process.env.REACT_APP_API_KEY;
+
+type AxoisProps = {
+  search_name: String;
+};
+
+const useAxios = ({ search_name }: AxoisProps) => {
+  const [data, setData] = useState([]);
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  const searchMovie = async (name: String) => {
+    await axios
+      .get(
+        `3/search/movie?api_key=${TMDB_KEY}&&language=ko-KR&page=1&query=${name}`,
+      )
+      .then((res) => {
+        const { results } = res.data;
+
+        setData(results);
+      })
+      .catch((error) => setError(error));
+
+    setLoading(false);
+  };
+
+  useEffect(() => {
+    searchMovie(search_name);
+  }, []);
+
+  return { data, error, loading };
+};
+
+export default useAxios;
