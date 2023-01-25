59faf5715c877c1c68ef0444fad436d4e8703dde
YouYou94 <ybh4115@gmail.com>
2023-01-25 19:13:27 +0900

    refactor: 검색을 활용하여 useAxios를 불러와야 하기 때문에 app에서는 라우터만 export한다.

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,7 @@
-import { useState, useEffect } from 'react';
 import AppRouter from './router/AppRouter';
 import './App.css';
-import axios from 'axios';
 
 function App() {
-  // Context API 사용하기
-  // 커스텀 훅 사용하기
-  const [movies, setMovies] = useState();
-
-  useEffect(() => {
-    axios
-      .get(
-        'https://api.themoviedb.org/3/discover/movie?api_key=69c4823b56b37a91a1ea5845cce9ce43',
-      )
-      .then((res) => console.log(res.data))
-      .catch((error) => console.log(error));
-  }, []);
-
   return (
     <div className="App">
       <AppRouter />
