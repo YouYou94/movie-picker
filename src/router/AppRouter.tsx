7d0cf60939fca4e416aa068f4258de22e901f0c1
YouYou94 <ybh4115@gmail.com>
2023-01-26 17:22:10 +0900

    export: Search Container

--- src/router/AppRouter.tsx
+++ src/router/AppRouter.tsx
@@ -1,8 +1,10 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes } from 'react-router-dom';
+import { SearchContainer } from '../components';
 
 const AppRouter = () => {
   return (
     <BrowserRouter>
+      <SearchContainer />
       <Routes></Routes>
     </BrowserRouter>
   );
