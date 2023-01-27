f692e9380484cda0d31d31bc206e94ddd7c6acef
YouYou94 <ybh4115@gmail.com>
2023-01-27 16:44:58 +0900

    Add: common Loading 구현

---
+++ src/components/common/Loading/Loading.tsx
@@ -0,0 +1,43 @@
+import styled, { keyframes } from 'styled-components';
+
+export const Loading = () => {
+  return (
+    <LoadingBox>
+      <LoadingMessage>잠시만 기다려주세요.</LoadingMessage>
+      <LoadingSpinner></LoadingSpinner>
+    </LoadingBox>
+  );
+};
+
+const Spin = keyframes`
+from {
+    transform: rotate(0deg);
+}
+to {
+    transform: rotate(359deg);
+}
+`;
+
+export const LoadingBox = styled.div`
+  flex: 1;
+
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+
+  gap: 1.5rem;
+`;
+
+export const LoadingMessage = styled.span``;
+
+export const LoadingSpinner = styled.div`
+  width: 8rem;
+  height: 8rem;
+
+  border: 5px solid rgb(245, 245, 245);
+  border-top: 5px solid rgb(45, 45, 45);
+  border-radius: 50%;
+
+  animation: ${Spin} 1.5s infinite linear;
+`;
