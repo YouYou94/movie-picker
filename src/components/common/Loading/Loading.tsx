5ad9a0631addeb7f6c206b499c6e8d60ececc327
YouYou94 <ybh4115@gmail.com>
2023-01-28 18:53:22 +0900

    refactor: 로딩 컴포넌트 파일 분리

--- src/components/common/Loading/Loading.tsx
+++ src/components/common/Loading/Loading.tsx
@@ -1,4 +1,4 @@
-import styled, { keyframes } from 'styled-components';
+import { LoadingBox, LoadingMessage, LoadingSpinner } from './LoadingStyled';
 
 export const Loading = () => {
   return (
@@ -8,36 +8,3 @@ export const Loading = () => {
     </LoadingBox>
   );
 };
-
-const Spin = keyframes`
-from {
-    transform: rotate(0deg);
-}
-to {
-    transform: rotate(359deg);
-}
-`;
-
-export const LoadingBox = styled.div`
-  flex: 1;
-
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-
-  gap: 1.5rem;
-`;
-
-export const LoadingMessage = styled.span``;
-
-export const LoadingSpinner = styled.div`
-  width: 8rem;
-  height: 8rem;
-
-  border: 5px solid rgb(245, 245, 245);
-  border-top: 5px solid rgb(45, 45, 45);
-  border-radius: 50%;
-
-  animation: ${Spin} 1.5s infinite linear;
-`;
