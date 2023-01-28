1fcd64a472075a45d6319e237b12bd5e006eafa3
YouYou94 <ybh4115@gmail.com>
2023-01-28 18:48:28 +0900

    Add: 페이지 생성 및 export

---
+++ src/pages/MoviePage.tsx
@@ -0,0 +1,5 @@
+const MoviePage = () => {
+  return <></>;
+};
+
+export default MoviePage;
---
+++ src/pages/index.tsx
@@ -0,0 +1,3 @@
+export { default as HomePage } from './HomePage';
+export { default as SearchPage } from './SearchPage';
+export { default as MoviePage } from './MoviePage';
