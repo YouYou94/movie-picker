1e2ddccce6031ecd47951b28bce668b0e117725c
YouYou94 <ybh4115@gmail.com>
2023-01-31 19:36:49 +0900

    Add: PopularMovies 컴포넌트 export

--- src/components/index.tsx
+++ src/components/index.tsx
@@ -1,9 +1,10 @@
 /* common components */
 export { Loading } from './common/loading/Loading';
 export { Search } from './common/search/Search';
-export { Layout } from './common/layout/Layout';
+export { Template } from './common/Template/Template';
 
 /* components */
 
 /* Home */
 export { HomeNav } from './Home/HomeNav/HomeNav';
+export { PopularMovies } from './Home/PopularMovies/PopularMovies';
