97b3ce66890212df2f708df28220059e3bc8dbc9
YouYou94 <ybh4115@gmail.com>
2023-02-08 14:03:37 +0900

    Feat: Nav 클릭 기능 구현 및 search 데이터 가져오기 성공

--- src/containers/Search/SearchContainer.tsx
+++ src/containers/Search/SearchContainer.tsx
@@ -1,11 +1,42 @@
-import { Header, Nav, PageTemplate, Search } from '../../components';
+import React, { useState, useEffect } from 'react';
+import { useNavigate, useParams } from 'react-router';
+import useAxios from '../../hooks/useAxios';
+import { Header, Loading, Nav, PageTemplate, Search } from '../../components';
+
+const POPULAR_URL = '/moviepicker/popular';
+const UPCOMING_URL = '/moviepicker/upcoming';
+const SEARCH = 'search/movie';
 
 const SearchContainer = () => {
+  const param = useParams();
+  const navigate = useNavigate();
+
+  /* Nav */
+
+  const onClickPopular = () => navigate(POPULAR_URL);
+
+  const onClickRecent = () => navigate(UPCOMING_URL);
+
+  /* Search */
+  const { movies, error, loading }: any = useAxios({
+    sub_url: SEARCH,
+    search_name: param.id ? param.id : '',
+  });
+
+  useEffect(() => {
+    if (error) alert(`에러 ! ${error}`);
+  }, [error]);
+
   return (
     <PageTemplate>
       <Header />
       <Search />
-      <Nav />
+      <Nav
+        searchKeyword={param.id ? param.id : ''}
+        onClickPopular={onClickPopular}
+        onClickRecent={onClickRecent}
+      />
+      {loading ? <Loading /> : <></>}
     </PageTemplate>
   );
 };
