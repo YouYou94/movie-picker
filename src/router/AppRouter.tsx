0c13d6edb4bb07e3581887d0e9fd833f36b429bd
YouYou94 <ybh4115@gmail.com>
2023-02-13 16:48:53 +0900

    리펙터링 시작

--- src/router/AppRouter.tsx
+++ src/router/AppRouter.tsx
@@ -17,25 +17,14 @@ const AppRouter = () => {
 export default AppRouter;
 
 /*
-페이지 라우터 설정
-
-/* : 해당하는 라우트가 없을 시 전부 /moviepicker url로 이동
+리펙터링
 
 /moviepicker
-- 메인 홈 url
-- 필요 컴포넌트 : moviepicker 타이틀 및 검색창 / 최신 영화 및 인기 영화
-
-/moviepicker/search/:id
-- 검색 영화
-- id에는 제목이 들어간다
-- 헤더 (왼쪽: 타이틀 / 중앙 : 검색창 / 오른쪽 : 메뉴)
-- 섹션 : 검색 내용들 (어떤식으로 표현할까? 캐러셀? 아니면 무한스크롤?)
-
-/moviepicker/movie/:id
-- 검색한 영화 중 하나를 클릭 시 해당하는 영화를 보여준다
-- id에는 제목 또는 해당 영화의 id가 들어간다
-- 헤더 (위와 동일)
-- 섹션 : 해당 영화의 디테일 정보
-
-그렇다면 페이지는 총 3개
+- 홈 URL
+- 구성
+    - Header (Fixed)
+    - Trailer (height 100vh)
+    - searchbar
+    - section (인기, 최신)
+    - footer
 */
