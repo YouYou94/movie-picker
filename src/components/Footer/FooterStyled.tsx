2f4f318ba92af97b2ef0f59f1a041a1a698076bf
YouYou94 <ybh4115@gmail.com>
2023-03-05 19:06:07 +0900

    Style: Footer 스타일 수정

--- src/components/Footer/FooterStyled.tsx
+++ src/components/Footer/FooterStyled.tsx
@@ -2,10 +2,24 @@ import styled from 'styled-components';
 
 export const Layout = styled.footer`
   width: 100%;
-
   height: 20rem;
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
 
   margin-top: 5rem;
 
   background-color: rgb(17, 17, 17);
+
+  gap: 1rem;
 `;
+
+export const FooterTitle = styled.h1`
+  font-family: 'Dosis';
+  font-weight: 700;
+
+  color: rgb(242, 242, 242);
+`;
+
+export const FooterMenuBox = styled.div``;
